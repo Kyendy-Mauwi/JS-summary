579e4ee8748031c78d76d562c6dbf4dc0e16dd68
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-30 12:21:59 +0300

    Create consoleLog.js
    How to print in JS. :thumbsup:

---
+++ consoleLog.js
@@ -0,0 +1,7 @@
+#console.log()
+
+/*console.log() method is used to log or print messages to the console. 
+It can also be used to print objects and other informatin to the screen.*/
+
+console.log('Hi there!');
+// Prints: Hi there!
