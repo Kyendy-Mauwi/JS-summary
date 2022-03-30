25b63b9ceb3743f1392316cef652d2fd3bc6e705
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-30 19:38:10 +0300

    Update and rename consoleLog.js to Cheatsheet.js

---
+++ Cheatsheet.js
@@ -0,0 +1,18 @@
+#console.log()
+/*
+console.log() method is used to log or print messages to the console. 
+It can also be used to print objects and other informatin to the screen.
+*/
+
+console.log('Hi there!');
+// Prints: Hi there!
+
+
+#Numbers
+/*
+These are a primitive data type.
+They consist of all integers and floating point numbers.
+*/
+
+let amount = 6; //interger
+let price = 6.99; //floating point number
--- consoleLog.js
+++
@@ -1,7 +0,0 @@
-#console.log()
-
-/*console.log() method is used to log or print messages to the console. 
-It can also be used to print objects and other informatin to the screen.*/
-
-console.log('Hi there!');
-// Prints: Hi there!
