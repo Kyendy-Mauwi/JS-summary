834290550d737ec5efa5fb5afbd60c91cb045e80
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-30 19:42:55 +0300

    Update Cheatsheet.js

--- Cheatsheet.js
+++ Cheatsheet.js
@@ -6,7 +6,7 @@ It can also be used to print objects and other informatin to the screen.
 
 console.log('Hi there!');
 // Prints: Hi there!
-
+---------------------------------------------------------------------------
 
 #Numbers
 /*
@@ -16,3 +16,16 @@ They consist of all integers and floating point numbers.
 
 let amount = 6; //interger
 let price = 6.99; //floating point number
+---------------------------------------------------------------------------
+
+#String.length
+/*
+```.length``` property of a string returns the number of caharacters that make up the string.
+*/
+
+let message = 'Hey there 😉!';
+console.log(message.length);
+// Prints: 11
+
+console.log('Kyendy'.length);
+// Prints: 5
