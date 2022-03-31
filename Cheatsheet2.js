7cf3e3f1acf9d95901eed3643897333dc3fd8815
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-31 09:14:50 +0300

    Create Cheatsheet2.js

---
+++ Cheatsheet2.js
@@ -0,0 +1,10 @@
+String Interpolation
+/*
+Is the process of evaluating string literals containing one or more placeholders (expressions, variables, etc).
+Performed using template literals: text ${expression} text.
+*/
+
+let height = 169;
+
+'Kyendy is '+ height + ' cm tall.'; //String concatenation
+`Kyendy is $height cm tall`; //String interpolation
