7455f6a9786722cabd3e038c86c5bc8d2f1cdd37
Kyendy_Gurama <62172250+Kyendy-Mauwi@users.noreply.github.com>
2022-03-31 12:10:47 +0300

    Update Cheatsheet2.js

--- Cheatsheet2.js
+++ Cheatsheet2.js
@@ -1,4 +1,4 @@
-String Interpolation
+#String Interpolation
 /*
 Is the process of evaluating string literals containing one or more placeholders (expressions, variables, etc).
 Performed using template literals: text ${expression} text.
@@ -8,3 +8,17 @@ let height = 169;
 
 'Kyendy is '+ height + ' cm tall.'; //String concatenation
 `Kyendy is $height cm tall`; //String interpolation
+---------------------------------------------------------------------------------
+
+#Variables
+/*
+Used whenever there’s a need to store a piece of data.
+Contain data that can be used in the program or elsewhere.
+They ensure code reusablity since they can be used to replace the same value in multiple places.
+*/
+
+const euro = '£';
+let income = 100000;
+
+console.log(euro + income + ' is more than avg income.');
+//Prints :£100000 is more than avg income.
